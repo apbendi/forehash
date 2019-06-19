bf3a492594c4199a420fd16c982b558c1c1fcac7
Ben DiFrancesco <ben@scopelift.co>
2019-06-19 11:49:43 -0400

    Update truffle config to work with ganache cli in dev

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,12 @@
 const path = require("path");
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "app/src/contracts"),
+	contracts_build_directory: path.join(__dirname, "app/src/contracts"),
+	networks: {
+		development: {
+			host: "127.0.0.1",
+			port: 8545,
+			network_id: "*",
+		}
+	},
 };
