f6cf48b768daa43b80b7c6e287da8cb3141e110d
Ben DiFrancesco <ben@scopelift.co>
2019-07-12 14:06:59 -0400

    Configure application for Ropsten test deployment

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,6 @@
+const testnet = require('./testnet-mneumonic');
 const path = require("path");
+const HDWalletProvider = require("truffle-hdwallet-provider");
 
 module.exports = {
 	contracts_build_directory: path.join(__dirname, "app/src/contracts"),
@@ -7,6 +9,13 @@ module.exports = {
 			host: "127.0.0.1",
 			port: 8545,
 			network_id: "*",
+		},
+		ropsten: {
+			network_id: '3',
+			gas: 4000000,
+			provider: () => {
+				return new HDWalletProvider(testnet.mneumonic, "https://ropsten.infura.io/v3/" + testnet.infura_id);
+			},
 		}
 	},
 };
