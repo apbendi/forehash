cf8692c5896121970ec0896ab9cad4206a444b5c
Ben DiFrancesco <ben@scopelift.co>
2019-07-16 22:43:49 -0400

    Simple styles for no metamask page

--- app/src/components/load_status/NoWeb3.js
+++ app/src/components/load_status/NoWeb3.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Card, Container, Row, Col } from 'react-bootstrap';
 
 const NoWeb3 = () => {
     return (
-        <div>
-            Web 3 not found
-        </div>
+        <Container className="mt-4">
+            <Row>
+                <Col xs="2"></Col>
+                <Col xs="8">
+                    <Card>
+                        <Card.Header as="h5"><span role="img" aria-label="fox emoji">🦊</span> Web3 Is Required</Card.Header>
+                        <Card.Body>
+                            <Card.Text>
+                                This website communicates with the Ethereum network via a protocol known as web3.
+                                To enable this site in your browser, download a web3 compatible browser extension
+                                like MetaMask.
+                            </Card.Text>
+                            <a href="https://metamask.io" className="btn btn-primary">Get MetaMask</a>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
