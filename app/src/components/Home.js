4c9a5dede93f0c35a54c3794e2e1a5ab20d95e62
Ben DiFrancesco <ben@scopelift.co>
2019-07-22 12:05:43 -0400

    Add a simple homepage
    * The tritest: jumbotron & three cards
    * Import react-icons

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Container, Jumbotron, Row, Col, Card, Button } from 'react-bootstrap';
+import { MdPublish, MdLockOutline } from 'react-icons/md';
+import { GoMegaphone } from 'react-icons/go';
 
 class Home extends Component {
     render() {
@@ -11,16 +12,57 @@ class Home extends Component {
         ];
 
         return (
-            <Container className="mt-3">
-                <Link to={"/" + links[0]}>{links[0]}</Link><br />
-                <Link to={"/" + links[1]}>{links[1]}</Link>
+            <Container className="mt-4">
+                <Row className="justify-content-center">
+                    <Col xs="12" md="9">
+                        <Jumbotron>
+                            <h2>Predict, Commit, Reveal</h2>
+                            <p>
+                                Pre-imaged predictions with skin in the game. Publication history is
+                                stored immutably on the Ethereum blockchain. A small deposit locked until
+                                each prediction is revealed. Add accountability to your public prognostications.
+                            </p>
+                            <LinkContainer to="/new">
+                                <Button>Get Started</Button>
+                            </LinkContainer>
+                        </Jumbotron>
+                    </Col>
+                </Row>
+                <Row className="justify-content-center">
+                    <Col xs="12" md="3" className="d-flex align-items-stretch">
+                        <Card>
+                            <Card.Body>
+                                <Card.Title><MdPublish /> Predict</Card.Title>
+                                <Card.Text>
+                                    Put a timestamped prediction hash to the Ethereum blockchain.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col xs="12" md="3" className="mt-4 mt-md-0">
+                        <Card>
+                            <Card.Body>
+                                <Card.Title><MdLockOutline /> Commit</Card.Title>
+                                <Card.Text>
+                                    Lock a small deposit to signal your committment to exposing it.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col xs="12" md="3" className="mt-4 mt-md-0">
+                        <Card>
+                            <Card.Body>
+                                <Card.Title><GoMegaphone /> Reveal</Card.Title>
+                                <Card.Text>
+                                    Reveal your prediction's plaintext to get your deposit back.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
             </Container>
         );
     }
-}
-
-Home.propTypes = {
-
 }
 
 export default Home;
