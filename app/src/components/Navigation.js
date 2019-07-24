0394ee3cf5f5d227010603677410f1b3077acaee
Ben DiFrancesco <ben@scopelift.co>
2019-07-24 12:34:44 -0400

    Add logo to navbar branding

--- app/src/components/Navigation.js
+++ app/src/components/Navigation.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Navbar, Nav } from 'react-bootstrap';
+import logo from '../full-text-logo.png';
 
 const Navigation = props => {
     let predictionsLink = props.account ? "/" + props.account : "/";
 
     return (
         <Navbar bg="light">
-            <Navbar.Brand href="/">Bankshot</Navbar.Brand>
+            <Navbar.Brand href="/">
+                <img src={logo}
+                        height="30"
+                        className="d-inline-block align-top"
+                        alt="Forehash Logo" />
+            </Navbar.Brand>
             <Nav.Link href="/activity">Activity</Nav.Link>
             <Nav.Link href="/new">New</Nav.Link>
             <Nav.Link href={predictionsLink}>My Predictions</Nav.Link>
