4759d2590d03a57e47b3cdb1cb9402a9823aa0a5
Ben DiFrancesco <ben@scopelift.co>
2019-07-24 13:25:32 -0400

    Improvements to the homepage to correspond to the new name
    * Show a dictionary-style definition of forehash in the jumbotron
    * Update icons and copy used in the cards describing the steps
    * Add a link icon to the "Get Started" button

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react'
 import { LinkContainer } from 'react-router-bootstrap';
 import { Container, Jumbotron, Row, Col, Card, Button } from 'react-bootstrap';
-import { MdPublish, MdLockOutline } from 'react-icons/md';
-import { GoMegaphone } from 'react-icons/go';
+import { GoMegaphone, GoLightBulb, GoLock, GoLinkExternal } from 'react-icons/go';
 
 class Home extends Component {
     render() {
@@ -10,15 +9,37 @@ class Home extends Component {
             <Container className="mt-4">
                 <Row className="justify-content-center">
                     <Col xs="12" md="9">
-                        <Jumbotron>
-                            <h2>Predict, Commit, Reveal</h2>
-                            <p>
-                                Pre-imaged predictions with skin in the game. Publication history is
-                                stored immutably on the Ethereum blockchain. A small deposit locked until
-                                each prediction is revealed. Add accountability to your public prognostications.
+                        <Jumbotron className="pt-3 pb-4">
+                                <h1 className="display-4 mb-0">forehash <span className="lead">/ ˈfoʊrhæʃ /</span></h1>
+                                <hr className="mt-0 mb-3"></hr>
+                            <p className="mb-1">
+                                <i style={{fontSize: "1.2em"}}>noun, plural <b>fore·hashes</b></i> <br />
                             </p>
-                            <LinkContainer to="/new">
-                                <Button>Get Started</Button>
+                            <div className="ml-4">
+                                    <p className="text-muted" style={{float: "left", height: "100%"}}>1</p>
+                                    <p className="ml-4">
+                                        The hash of a prediction which is distributed publicly, such that the prediction
+                                        itself can be revealed after it is proven correct or incorrect.
+                                    </p>
+                            </div>
+                            <div className="ml-4">
+                                    <p className="text-muted" style={{float: "left", height: "100%"}}>2</p>
+                                    <p className="ml-4 mb-0">
+                                        A commitment to reveal some knowledge in the future by publishing its hash
+                                        in the present.
+                                    </p>
+                            </div>
+                            <p className="mb-1">
+                                <i style={{fontSize: "1.2em"}}>verb</i> <br />
+                            </p>
+                            <div className="ml-4">
+                                    <p className="text-muted" style={{float: "left", height: "100%"}}>1</p>
+                                    <p className="ml-4">
+                                        The act of publishing a forehash.
+                                    </p>
+                            </div>
+                            <LinkContainer className="mt-2" to="/new">
+                                <Button size="lg">Get Started <GoLinkExternal /></Button>
                             </LinkContainer>
                         </Jumbotron>
                     </Col>
@@ -27,29 +48,31 @@ class Home extends Component {
                     <Col xs="12" md="3" className="d-flex align-items-stretch">
                         <Card>
                             <Card.Body>
-                                <Card.Title><MdPublish /> Predict</Card.Title>
+                                <Card.Title><GoLightBulb /> Predict</Card.Title>
                                 <Card.Text>
-                                    Put a timestamped prediction hash to the Ethereum blockchain.
+                                    Write a short prediction you'd like to reveal publicly at some point
+                                    in the future.
                                 </Card.Text>
                             </Card.Body>
                         </Card>
                     </Col>
-                    <Col xs="12" md="3" className="mt-4 mt-md-0">
+                    <Col xs="12" md="3" className="mt-4 mt-md-0 d-flex align-items-stretch">
                         <Card>
                             <Card.Body>
-                                <Card.Title><MdLockOutline /> Commit</Card.Title>
+                                <Card.Title><GoLock /> Commit</Card.Title>
                                 <Card.Text>
-                                    Lock a small deposit to signal your committment to exposing it.
+                                    Publish a hash of your prediciton to the Ethereum blockchain, along with a
+                                    small deposit.
                                 </Card.Text>
                             </Card.Body>
                         </Card>
                     </Col>
-                    <Col xs="12" md="3" className="mt-4 mt-md-0">
+                    <Col xs="12" md="3" className="mt-4 mt-md-0 d-flex align-items-stretch">
                         <Card>
                             <Card.Body>
                                 <Card.Title><GoMegaphone /> Reveal</Card.Title>
                                 <Card.Text>
-                                    Reveal your prediction's plaintext to get your deposit back.
+                                    Reveal your prediction's plaintext and get your deposit back.
                                 </Card.Text>
                             </Card.Body>
                         </Card>
