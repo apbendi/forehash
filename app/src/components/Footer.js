c90b5fe1dfe823b9374c6f030c79653efb464448
Ben DiFrancesco <ben@scopelift.co>
2019-07-24 12:45:14 -0400

    Add icon attribution to footer

--- app/src/components/Footer.js
+++ app/src/components/Footer.js
@@ -6,10 +6,14 @@ import { FaCopyright } from 'react-icons/fa';
 const Footer = _props => {
     return (
         <Navbar bg="light" fixed="bottom">
-            <p className="text-muted">
+            <p className="text-secondary">
                 <GoPerson /> <span style={{fontSize: ".8em"}}>Built for fun by <a href="https://twitter.com/bendifrancesco" target="blank">@BenDiFrancesco</a></span><br />
                 <GoMarkGithub /> <span style={{fontSize: ".8em"}}>Code availble on <a href="https://github.com/apbendi/bankshot" target="blank">GitHub</a></span><br />
                 <FaCopyright /> <span style={{fontSize: ".8em"}}>2019 All Rights Reserved </span><br />
+                <span className="text-muted" style={{fontSize: ".7em"}}>
+                    Icon derivative from <a href="https://www.flaticon.com/authors/freepik" target="blank">freepik</a>
+                    (<a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>)
+                </span>
             </p>
         </Navbar>
     )
