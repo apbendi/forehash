15f9c511cddcf10f7cf307fe091193a9cfdddef8
Ben DiFrancesco <ben@scopelift.co>
2019-07-24 22:24:36 -0400

    Update migration params for main contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,8 @@
 const Bankshot = artifacts.require("Bankshot");
 
 module.exports = function(deployer) {
-  let ethVig = web3.utils.toWei('0.05', 'ether');
-  let minEthDeposit = web3.utils.toWei('0.01', 'ether');
+  let ethVig = web3.utils.toWei('0.0', 'ether');
+  let minEthDeposit = web3.utils.toWei('0.02', 'ether');
 
   deployer.deploy(Bankshot, ethVig, minEthDeposit);
 };
