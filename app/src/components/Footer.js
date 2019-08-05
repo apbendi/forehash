a266ab042a03ae866f5e602cbdece4cd4e7a7bfe
Ben DiFrancesco <ben@scopelift.co>
2019-08-05 14:59:00 -0400

    Inlcude frontend version number in the interface

--- app/src/components/Footer.js
+++ app/src/components/Footer.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Navbar } from 'react-bootstrap';
 import { GoPerson, GoMarkGithub } from 'react-icons/go';
 import { FaCopyright } from 'react-icons/fa';
+import packageInfo from '../../package.json';
 
 const Footer = _props => {
     return (
         <Navbar bg="light" fixed="bottom" className="footer pb-0">
             <p className="text-secondary pb-0 mb-0 mt-0">
                 <GoPerson /> <span style={{fontSize: ".8em"}}>Built for fun by <a href="https://twitter.com/bendifrancesco" target="_blank" rel="noopener noreferrer">@BenDiFrancesco</a></span><br />
-                <GoMarkGithub /> <span style={{fontSize: ".8em"}}>Code available on <a href="https://github.com/apbendi/forehash" target="_blank" rel="noopener noreferrer">GitHub</a></span><br />
+                <GoMarkGithub /> <span style={{fontSize: ".8em"}}>Code available on <a href="https://github.com/apbendi/forehash" target="_blank" rel="noopener noreferrer">GitHub</a> (v{packageInfo.version})</span><br />
                 <FaCopyright /> <span style={{fontSize: ".8em"}}>2019 All Rights Reserved </span><br />
                 <span className="text-muted" style={{fontSize: ".7em"}}>
                     Icon derivative from <a href="https://www.flaticon.com/authors/freepik" target="blank">freepik</a>
