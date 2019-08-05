b6d9f253701c166ec367aa2c23a55f8f1f787def
Ben DiFrancesco <ben@scopelift.co>
2019-08-05 12:06:22 -0400

    Avoid the invalid network message when in development mode on ganache-cli

--- app/src/components/InterfaceComponent.js
+++ app/src/components/InterfaceComponent.js
@@ -15,8 +15,9 @@ import NoWeb3 from './load_status/NoWeb3';
 import InvalidNetwork from './load_status/InvalidNetwork';
 
 function isValidNetwork(networkId) {
-  //return networkId === 1 || networkId === 3 || networkId === 5777 || networkId > 1500000000000;
-  return networkId === 1;
+
+  return networkId === 1 ||
+            (window.location.href.includes("localhost") && networkId > 1500000000000);
 }
 
 const InterfaceComponent = props => {
